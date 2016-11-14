479441963dd4ec25fc0ca3d3514428a8409ae340
Bryce Stradling <brycestradling@gmail.com>
2016-11-14 17:19:58 -0600

    moved section animation to pure css, updated some of the nav

--- js/main.js
+++ js/main.js
@@ -43,47 +43,47 @@ $(document).ready(function() {
         onSlideLeave: function(anchorLink, index, slideIndex, direction, nextSlideIndex){}
     });
     // Scrollex
-    $(function() {
-      $('.container').scrollex({
-        enter: function() {
-
-          // Set #foobar's background color to green when we scroll into it.
-            $(this).css('opacity', 1);
-            $(this).css('animation', 'bounce 3s ease-out');
-            $(this).css('transform', 'translateX(0px)');
-        },
-        leave: function() {
-
-          // Reset #foobar's background color when we scroll out of it.
-            $(this).css('opacity', 0);
-            $(this).css('transform', 'translateX(-500px)')
-        }
-    });
-    $('#clock_icon').scrollex({
-      enter: function() {
-        $(this).css('transform','rotate(30deg)');
-      },
-      leave: function() {
-        $(this).css('transform','rotate(0deg)');
-      }
-    });
-    $('#code').scrollex({
-      enter: function() {
-        $(this).css('animation', 'down 1s linear 1s forwards')
-      }
-    });
-    $('#sketch').scrollex({
-      enter: function() {
-        $(this).css('animation', 'down 1s linear 1.5s forwards')
-      }
-    });
-    $('#analytics').scrollex({
-      enter: function() {
-        $(this).css('animation', 'down 1s linear 2s forwards')
-      }
-    });
-
-  }); // \\Scrollex
+  //   $(function() {
+  //     $('.container').scrollex({
+  //       enter: function() {
+  //
+  //         // Set #foobar's background color to green when we scroll into it.
+  //           $(this).css('opacity', 1);
+  //           $(this).css('animation', 'bounce 3s ease-out');
+  //           $(this).css('transform', 'translateX(0px)');
+  //       },
+  //       leave: function() {
+  //
+  //         // Reset #foobar's background color when we scroll out of it.
+  //           $(this).css('opacity', 0);
+  //           $(this).css('transform', 'translateX(-500px)')
+  //       }
+  //   });
+  //   $('#clock_icon').scrollex({
+  //     enter: function() {
+  //       $(this).css('transform','rotate(30deg)');
+  //     },
+  //     leave: function() {
+  //       $(this).css('transform','rotate(0deg)');
+  //     }
+  //   });
+  //   $('#code').scrollex({
+  //     enter: function() {
+  //       $(this).css('animation', 'down 1s linear 1s forwards')
+  //     }
+  //   });
+  //   $('#sketch').scrollex({
+  //     enter: function() {
+  //       $(this).css('animation', 'down 1s linear 1.5s forwards')
+  //     }
+  //   });
+  //   $('#analytics').scrollex({
+  //     enter: function() {
+  //       $(this).css('animation', 'down 1s linear 2s forwards')
+  //     }
+  //   });
+  //
+  // }); // \\Scrollex
   $('.drawer-open').click(function(){
 		$('#navigation-bar').toggleClass('mobilenav');
 	})
